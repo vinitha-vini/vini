013237ccc15eafa9748653c257de7fdc898c600f
vinitha-vini <vinithav941@gmail.com>
2017-04-24 16:09:05 +0530

    Create largest no.c

---
+++ largest no.c
@@ -0,0 +1,13 @@
+# vini
+#include<stdio.h>
+int main()
+{
+int a=5;b=10;c=20;
+if(a>b && a>c)
+prinf("a is largest number");
+elseif(b>a && b>c)
+printf("b is largest number");
+elseif(c>a && c>b)
+printf("c is largest number");
+return 0;
+}
