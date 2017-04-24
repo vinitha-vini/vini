fe1e96178803fb8598f0498b87832bde08a18972
vinitha-vini <vinithav941@gmail.com>
2017-04-24 14:40:40 +0530

    Create vowel or constant.c

---
+++ vowel or constant.c
@@ -0,0 +1,12 @@
+# vini
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter a letter:");
+scanf("%s",ch);
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+printf("vowel");
+else
+printf("constant");
+}
