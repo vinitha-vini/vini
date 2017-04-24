dd8035d6751666627cb0ba1783ce835b8a6c3423
vinitha-vini <vinithav941@gmail.com>
2017-04-24 11:06:02 +0530

    Create even or odd .c

---
+++ even or odd .c
@@ -0,0 +1,12 @@
+# vini
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int number;
+printf("enter a no:");
+scanf("d",number);
+(number%==0)?(printf("positive number"):printf("negative number")
+getch();
+return 0;
+}
