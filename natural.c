aa90e9b267acdd5af1674f27b5a9a72f20953a8b
vinitha-vini <vinithav941@gmail.com>
2017-04-26 13:41:52 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,14 @@
+# vini
+#include<stdio.h>
+void main()
+{
+int i=1;sum=0;n;
+printf("enter an integer:");
+scanf("%d",n);
+while(i<=n)
+{
+sum=sum+i;
+i++;
+}
+printf("sum of natural no is:%d",sum);
+}
